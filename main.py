6efa6b453c4d4f0434b48934f3b867b13dcd6526
bilal1234-jpg <137507728+bilal1234-jpg@users.noreply.github.com>
2023-08-28 01:06:00 +0500

    ok1

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from forms import CreatePostForm, reg_form,login_form,comments
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -31,7 +31,7 @@ gravatar = Gravatar(app, size=50,
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'instance', 'post3.db')
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get("DB_URI",'sqlite:///' + os.path.join(app.root_path, 'instance', 'post3.db'))
 db = SQLAlchemy()
 db.init_app(app)
 
