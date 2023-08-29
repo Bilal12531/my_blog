309f17350b9ec21e9a20d0f240c530520e6aae8f
bilal1234-jpg <137507728+bilal1234-jpg@users.noreply.github.com>
2023-08-30 02:42:17 +0500

    finalday

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('login', logout = True))
 
 
 @app.route('/home')
